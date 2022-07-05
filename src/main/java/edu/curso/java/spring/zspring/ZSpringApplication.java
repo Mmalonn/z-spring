8a2f4b04c38317948953d14eb7b46cc82fe6a922
marcos torres <marcosezequiel_torres@hotmail.com>
2022-07-05 19:31:16 -0300

    iniciando proyecto trabajos de obra

---
+++ src/main/java/edu/curso/java/spring/zspring/ZSpringApplication.java
@@ -0,0 +1,14 @@
+package edu.curso.java.spring.zspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZSpringApplication.class, args);
+		System.out.println("Arriba");
+	}
+
+}
---
+++ src/test/java/edu/curso/java/spring/zspring/ZSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.curso.java.spring.zspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
