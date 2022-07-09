b13a36a207dd7d07540c49ad02abb79200e48803
marcos torres <marcosezequiel_torres@hotmail.com>
2022-07-09 19:33:48 -0300

    guardando antes de tocar trabajo y trabajo terminado

--- src/main/java/edu/curso/java/spring/zspring/rest/TrabajoRestController.java
+++ src/main/java/edu/curso/java/spring/zspring/rest/TrabajoRestController.java
@@ -6,8 +6,13 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,18 +26,31 @@ import edu.curso.java.spring.zspring.service.interf.TrabajoService;
 public class TrabajoRestController {
 
 	private static Logger log = LoggerFactory.getLogger(TrabajoRestController.class);
-	
+
 	@Autowired
-	private TrabajoService trabajoservice;
-	
+	private TrabajoService trabajoService;
+
 	@GetMapping("/trabajos")
-	public List<TrabajoDto> listarTrabajos(){
-		List<TrabajoBo> trabajos = trabajoservice.listarTrabajos();
+	public List<TrabajoDto> listarTrabajos() {
+		List<TrabajoBo> trabajos = trabajoService.listarTrabajos();
 		List<TrabajoDto> trabajosDto = new ArrayList<TrabajoDto>();
-		for(TrabajoBo trabajo: trabajos) {
+		for (TrabajoBo trabajo : trabajos) {
 			trabajosDto.add(new TrabajoDto(trabajo));
 		}
 		log.info("mostrando trabajos");
 		return trabajosDto;
 	}
+
+	@GetMapping("/trabajos/{id}")
+	public ResponseEntity<TrabajoDto> obtenerTrabajo(@PathVariable Long id) {
+		TrabajoBo trabajo = trabajoService.obtenerTrabajo(id);
+		TrabajoDto trabajoDto = new TrabajoDto(trabajo);
+		return ResponseEntity.ok(trabajoDto);
+	}
+	
+//	@PostMapping("/trabajos")
+//	public ResponseEntity<TrabajoDto> nuevoTrabajo(@RequestBody TrabajoDto trabajoDto){
+//		TrabajoBo trabajo = new TrabajoBo();
+//		return ResponseEntity.status(HttpStatus.CREATED).body(trabajoDto);
+//	}
 }
